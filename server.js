47bf27117dc9a98a4f12659a8b02b21b8432a257
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-16 16:58:03 -0700

    basic request successfully routed

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const http = require('http');
+const app = require('./lib/app');
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    console.log('Request heard!');
-});
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log('server started on port', server.address().port); // eslint-disable-line
