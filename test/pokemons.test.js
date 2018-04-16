cb94da43399837c86f6525dfaa72379420925f1e
Steele Walston <steele.walston@gmail.com>
2018-04-16 16:34:50 -0700

    ready for testing

---
+++ test/pokemons.test.js
@@ -0,0 +1,21 @@
+const { assert } = require('chai');
+const request = require('./request');
+const Pokemon = require('../lib/models/pokemon');
+
+describe('Pokemen', () => {
+
+    let bulb = {
+        name: 'Bulbasaur',
+        type: 'Grass'
+    };
+
+    let char = {
+        name: 'Charmander',
+        type: 'Fire'
+    };
+
+    it('saves a pokeman', () => {
+        
+    });
+
+});
