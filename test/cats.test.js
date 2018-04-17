d7114e59be16942f98f955b1be016550309c5813
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-16 17:33:25 -0700

    test functioning on most basic level

---
+++ test/cats.test.js
@@ -0,0 +1,9 @@
+const { assert } = require('chai');
+const request = require('./request');
+const Cat = require('../lib/models/model');
+
+describe('Cat API', () => {
+    it('runs a test', () => {
+        assert.ok(true);
+    });
+});
