71ae27f27c602716ff16fc6b8f4c3891375dfbbe
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-17 09:30:29 -0700

    server works

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.creatServer(app);
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log('I am running fuckity fuck.');
