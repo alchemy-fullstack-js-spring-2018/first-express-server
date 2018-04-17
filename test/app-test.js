770f965e0eddc02421b21f4276ac34b1ce706c49
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-17 10:05:14 -0700

    save test fail

--- test/app-test.js
+++ test/app-test.js
@@ -1,1 +1,32 @@
-const app = require('../lib/app');
+const { assert } = require('chai');
+const Gems = require('../lib/models/gem');
+const request = require('./request');
+
+describe('E2E Gems', () => {
+
+    let garnet = {
+        name: 'Garnet',
+        type: 'Fusion',
+        weapon: 'Gauntlets'
+    };
+
+    let pearl = {
+        name: 'Pearl',
+        type: 'Organic',
+        weapon: 'Spear'
+    };
+
+
+
+
+    it('saves a Crystal Gem', () => {
+        return request.post('/gems')
+            .send(garnet)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.equal(body, { _id: body._id, ...pearl });
+            });
+    });
+
+
+});
