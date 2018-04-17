b9c6088582f3308ce4d34ec2fca045ea6fb95a2b
CharlyWelch <blwbiology@gmail.com>
2018-04-17 09:50:31 -0700

    clean up code, add server.js

--- test/museums.test.js
+++ test/museums.test.js
@@ -3,25 +3,16 @@ const request = require('./request');
 const Museum = require('../lib/models/model');
 
 describe('Museum API', () => {
+    
     let smithsonian = {
         name: 'Smithsonian',
         location: 'Washington, DC'
     };
+
     let met = {
         name: 'Metropolitan Museum of Art',
         location: 'New York City, NY'
     };
-    let harvard = {
-        name: 'Harvard Museum of Natural History',
-        location: 'Boston, MA'
-    };
-
-    // it('saves and gets a museum', () => {
-    //     return Museum.save(smithsonian)
-    //         .then(saved => {
-    //             assert.deepEqual(saved, { _id: saved._id, ...smithsonian })
-    //         });
-    // });
 
     it('saves a museum', () => {
         return request.post('/museums')
