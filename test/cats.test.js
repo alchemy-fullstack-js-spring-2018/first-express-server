996d3c6609760c325c9cd816f0e86aad8e59778e
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-16 17:50:46 -0700

    "post" method passes

--- test/cats.test.js
+++ test/cats.test.js
@@ -4,15 +4,18 @@ const Cat = require('../lib/models/model');
 
 describe('Cat API', () => {
 
-    const snowLeopard = {
+    let snowLeopard = {
         commonName: 'snow leopard',
         scientificName: 'Panthera uncia'
     };
 
-    it('saves and gets a cat', () => {
-        return Cat.save(snowLeopard)
-            .then(saved => {
-                assert.deepEqual(saved, { _id: saved._id, ...snowLeopard });
+    it('saves a cat (POST)', () => {
+        return request.post('/cats')
+            .send(snowLeopard)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.deepEqual(body, { _id: body._id, ...snowLeopard });
+                snowLeopard = body;
             });
     });
 });
