9b190303a4bc68105896f99a8ee6ce74e4872420
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-17 10:26:56 -0700

    created server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+/*eslint no-console: off */
+
+const http = require('http');
+const app = require('.lib/app');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+
+server.listen(PORT, () => {
+    console.log('server hears you', server.address().port());
+});
