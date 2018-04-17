fa8cc66d244caf82c7d8e82bf9ec83e8d5ea7a5f
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-16 17:01:03 -0700

    post and get working

--- routes/heroes.js
+++ routes/heroes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
-const Hero = require('../models/model');
+const Hero = require('../lib/models/model');
 const errorHandler = require('../error-handler');
 
 // save, findById, findByIdAndUpdate, findAll, findByIdAndRemove
+
+module.exports = router
+    .post('/', (req, res) => {
+        Hero.save(req.body)
+            .then(hero => res.json(hero));
+    })
+    .get('/', (req, res) => {
+        Hero.find()
+            .then(heroes => res.json(heroes))
+    });
--- test/heroes.test.js
+++ test/heroes.test.js
@@ -1,3 +1,37 @@
 const { assert } = require('chai');
 const request = require('./request');
 const Hero = require('../lib/models/model');
+
+describe('Overwatch API', () => {
+
+    let genji = {
+        name: 'Genji Schimada',
+        alias: 'Genji',
+        age: 35,
+        role: 'Flanker'
+    };
+    let zen = {
+        name: 'Tekhartha Zenyatta',
+        alias: 'Zenyatta',
+        age: 20,
+        role: 'Support'
+    };
+
+    it('saves a hero', () => {
+        return request.post('/heroes')
+            .send(genji)
+            .then(({ body }) => {
+                console.log(body);
+                assert.ok(body._id);
+                genji = body;
+            });
+    });
+
+    it('gets all heroes', () => {
+        return request.get('/heroes')
+            .then(({ body }) => {
+                assert.deepEqual(body, [genji]);
+            });
+    });
+
+});
