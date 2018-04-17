739e20a99543bb99eb0ef04c8607a91616f72b58
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-17 09:35:02 -0700

    updated post test to check if the body matched the pct object

--- test/trails.test.js
+++ test/trails.test.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 const request = require('./request');
-const trail = require('../lib/models/trail');
+// const trail = require('../lib/models/trail');
 
 describe('Trails API', () => {
     let pct = {
@@ -8,21 +8,23 @@ describe('Trails API', () => {
         length: 2650
     };
 
-    let at = {
-        name: 'Appalachian Trail',
-        length: 2181
-    };
+    // let at = {
+    //     name: 'Appalachian Trail',
+    //     length: 2181
+    // };
 
-    let cdt = {
-        name: 'Continental Divide Trail',
-        length: 3100
-    };
+    // let cdt = {
+    //     name: 'Continental Divide Trail',
+    //     length: 3100
+    // };
 
     it('saves a trail', () => {
         return request.post('/trails')
             .send(pct)
             .then(({ body }) => {
                 assert.ok(body._id);
+                assert.deepEqual(body, { _id: body._id, ...pct });
+                pct = body;
             });
     });
 });
