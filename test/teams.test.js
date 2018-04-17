a44923b4ecc78755168a5d0b4feb24bb028fee08
Jack Lomax <lomax15@me.com>
2018-04-17 11:39:12 -0700

    saves teams and test passes

--- test/teams.test.js
+++ test/teams.test.js
@@ -25,18 +25,18 @@ describe('team', () => {
     it('gets all teams', () => {
         return request.get('/teams')
             .then(({ body }) => {
-                assert.equal(body, [Seahawks, Rams, Cardinals]);
+                assert.deepEqual(body, [Seahawks, Rams, Cardinals]);
             });
     });
 
-    // it('saves a team', () => {
-    //     return request.post('/teams')
-    //         .send(Niners)
-    //         .then(({ body }) => {
-    //             assert.ok(body._id);
-    //             assert.deepEqual(body, { _id: body._id, ...Niners });
-    //             Niners = body;
-    //         });
-    // });
+    it('saves a team', () => {
+        return request.post('/teams')
+            .send(Cardinals)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.deepEqual(body, { _id: body._id, ...Cardinals });
+                Cardinals = body;
+            });
+    });
 
 });
