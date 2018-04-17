9eef0cadd60968d8e332bdcef5cec2da9788bdbe
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-17 10:28:46 -0700

    get and get by id tests fail

--- test/app-test.js
+++ test/app-test.js
@@ -16,15 +16,30 @@ describe('E2E Gems', () => {
         weapon: 'Spear'
     };
 
-
-
-
     it('saves a Crystal Gem', () => {
         return request.post('/gems')
             .send(garnet)
             .then(({ body }) => {
                 assert.ok(body._id);
                 assert.deepEqual(body, { _id: body._id, ...garnet });
+            });
+    });
+
+    it('gets a Crystal Gem by id', () => {
+        return Gems.save(pearl)
+            .then(saved => {
+                pearl = saved;
+                return request.get(`/gems/${pearl._id}`);
+            })
+            .then(({ body }) => {
+                assert.deepEqual(body, garnet);
+            });
+    });
+
+    it('gets all Crystal Gems', () => {
+        return request.get('/gems')
+            .then(({ body }) => {
+                assert.deepEqual(body, [pearl, pearl]);
             });
     });
 
