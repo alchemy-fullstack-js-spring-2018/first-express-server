d066675b00e81c69c73c31d21efb13cf035c6c02
pereztjacob <pereztjacob@gmail.com>
2018-04-17 09:20:08 -0700

    write chai test for post

---
+++ test/pets.test.js
@@ -0,0 +1,34 @@
+const { assert } = require('chai');
+const request = require('./request');
+const Pet = require('../lib/models/model');
+
+describe('pets API', () => {
+
+    let tigerShark = {
+        name: 'tiger',
+        size: 'decent',
+    };
+
+    let rhino = {
+        name: 'rhinocerus',
+        size: 'big'
+    };
+
+    it('posts pet', () => {
+        return request
+            .post('/pets')
+            .send(rhino)
+            .then(({ body }) => {
+                const { _id } = body;
+                assert.ok(_id);
+                assert.deepEqual(body, { _id, ...rhino });
+                rhino = body;
+                return _id;
+            })
+            .then(_id => Pet.findById(_id))
+            .then(found => {
+                assert.deepEqual(found, rhino);
+            });
+    });
+
+});
