25cd2f143bf64fc35c8bb78f36cf73503eea28be
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-16 17:40:22 -0700

    "save" method in model passes

--- test/cats.test.js
+++ test/cats.test.js
@@ -3,7 +3,16 @@ const request = require('./request');
 const Cat = require('../lib/models/model');
 
 describe('Cat API', () => {
-    it('runs a test', () => {
-        assert.ok(true);
+
+    const snowLeopard = {
+        commonName: 'snow leopard',
+        scientificName: 'Panthera uncia'
+    };
+
+    it('saves and gets a cat', () => {
+        return Cat.save(snowLeopard)
+            .then(saved => {
+                assert.deepEqual(saved, { _id: saved._id, ...snowLeopard });
+            });
     });
 });
