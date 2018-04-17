d81cc1ed9447800effaa3afa6eb0dd53ed7420e5
victorbofill <victorm.bofill@gmail.com>
2018-04-16 17:24:46 -0700

    Progress on save test

---
+++ test/friends.test.js
@@ -0,0 +1,27 @@
+const { assert } = require('chai');
+const request = require('./request');
+const Friend = require('../lib/models/friend');
+
+describe('Friends Express server', () => {
+    let sam = {
+        name: 'Sam',
+        weaponOfChoice: 'Literally anything',
+        catchphrase: 'When reason fails, force prevails'
+    };
+
+    let kasey = {
+        name: 'Kasey T. Whiler',
+        weaponOfChoice: 'Projectiles',
+        catchphrase: '*censored*'
+    };
+
+    it('Saves a new friend', () => {
+        return request.post('/friends')
+            .send(sam)
+            .then(( { body }) => {
+                assert.ok(body._id);
+                assert.deepEqual(body.model, {_id: body._id, ...sam });
+                sam = body;
+            })
+    });
+});
